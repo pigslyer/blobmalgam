06cb1ffaca18e74853a82d388ac99ca085092875
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2025-01-12 19:59:31 +0100

    Added spring, damping smoothening

--- src/ragdoll/player_amalgam.gd
+++ src/ragdoll/player_amalgam.gd
@@ -1,9 +1,11 @@
 extends Node2D
-class_name Amalgam
+class_name Amalgam_test
 
 var is_dragging: bool = false;
 var dragged_body: RigidBody2D = null;
-@export var drag_force_multiplier: float = 0.5;
+@export var drag_force_multiplier: float = 50;
+@export var spring_constant: float = 10.0
+@export var damping: float = 5.0
 
 func _ready() -> void:
 	# get_viewport().set_physics_object_picking_sort(true);
@@ -19,17 +21,12 @@ func _input(event: InputEvent) -> void:
 func _on_blob_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.is_action_pressed("click"):
-			start_drag();
+			start_drag(self.get_node("Blob"));
 			modulate = Color(1, 1, 1, 0.5);
 
-func start_drag() -> void:
-	var mouse_pos = get_viewport().get_mouse_position();
-	var space_state = get_world_2d().direct_space_state;
-	var parameters = PhysicsPointQueryParameters2D.new();
-	parameters.set_position(mouse_pos);
-	var result = space_state.intersect_point(parameters, 1)[0];
+func start_drag(node: RigidBody2D) -> void:
+	dragged_body = node;
 	is_dragging = true;
-	dragged_body = result.collider as RigidBody2D;
 
 func stop_drag() -> void:
 	is_dragging = false;
@@ -43,9 +40,12 @@ func _on_blob_mouse_exited() -> void:
 	pass ;
 	# print_debug("Mouse STOP hovering over blob");
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if is_dragging and dragged_body:
-		# print_debug("applying force to dragged body");
 		var mouse_pos = get_viewport().get_mouse_position();
-		var force = (mouse_pos - dragged_body.global_position) * drag_force_multiplier;
-		dragged_body.apply_central_impulse(force);
+		var distance = mouse_pos - dragged_body.global_position;
+		var velocity = dragged_body.linear_velocity;
+		var spring_force = distance * spring_constant;
+		var damping_force = -velocity * damping;
+		var total_force = spring_force + damping_force;
+		dragged_body.apply_force(total_force);
