7539a4fd5388e760b31951e4a88f37436c135178
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2025-01-19 13:04:54 +0100

    Hook up analgam signals

--- src/ragdoll/blob_display/blob_display.gd
+++ src/ragdoll/blob_display/blob_display.gd
@@ -18,18 +18,21 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 		if event.is_action_pressed("click"):
 			# Emitted on all blobs (even dead)
 			print_debug("Emitting blob_pressed with blob: ", self.name);
-			emit_signal("blob_pressed", card);
+			blob_pressed.emit(card);
 			# start_drag(self);
 			modulate = Color(1, 1, 1, 0.5);
 		elif event.is_action_released("click"):
 			# stop_drag();
 			modulate = Color(1, 1, 1, 1);
 
+func _process(delta: float) -> void:
+	pass ;
+
 func _on_mouse_entered() -> void:
 	# Emitted on all blobs (even dead)
 	print_debug("Emitting blob_hovered (state: true) with blob: ", self.name);
-	emit_signal("blob_hovered", card, true);
+	blob_hovered.emit(card, true);
 
 func _on_mouse_exited() -> void:
 	print_debug("Emitting blob_hovered (state: false) with blob: ", self.name);
-	emit_signal("blob_hovered", card, false);
+	blob_hovered.emit(card, false);
--- src/ragdoll/limb_display/limb_display.gd
+++ src/ragdoll/limb_display/limb_display.gd
@@ -17,7 +17,7 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 		if event.is_action_pressed("click"):
 			# Emitted on all limbs (even dead)
 			print_debug("Emitting limb_pressed with blob: ", self.name);
-			emit_signal("limb_pressed", self);
+			emit_signal("limb_pressed", self.card);
 			# start_drag(self);
 			modulate = Color(1, 1, 1, 0.5);
 		elif event.is_action_released("click"):
