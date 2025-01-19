03f07dd5264ba8fccf878ce848c00a9e660d256e
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2025-01-19 21:44:54 +0100

    Final

--- src/upgrade/screen.gd
+++ src/upgrade/screen.gd
@@ -5,13 +5,18 @@ signal finished;
 
 @onready var display: AmalgamDisplay = %Player;
 @onready var count: Label = %CountLabel;
+@onready var origin: Control = %Origin;
+
 var new_items := []
 var current_ix := 0;
 var preview_display: Control = null;
+var is_active := false;
 
 
 func upgrade(amalgam: Amalgam, new_blobs_or_limbs: Array) -> void:
+	is_active = true;
 	display.display_amalgam(amalgam);
+	display.card = amalgam;
 	new_items = new_blobs_or_limbs;
 	%CountLabel.text = "%d/%d body parts upgraded" % [current_ix, len(new_items)];
 	current_ix = 0;
@@ -27,8 +32,9 @@ func setup() -> void:
 		preview_display = %NewLimbDisplay;
 	preview_display.visible = true;
 
-func process_next_item() -> void:
+func process_next_item(gl_pos: Vector2) -> void:
 	if current_ix >= len(new_items):
+		is_active = false;
 		finished.emit();
 		return ;
 	
@@ -40,13 +46,28 @@ func process_next_item() -> void:
 	%NewLimbDisplay.visible = false;
 	if new_item is Blob:
 		preview_display = %NewBlobDisplay;
+		display.card.blobs.push_back(new_item);
+		var link := Amalgam.Link.new();
+		link.from_idx = 0;
+		link.to_idx = current_ix;
+		link.from_local_pos = Vector2.ZERO;
+		link.to_local_pos = gl_pos - origin.global_position;
+		display.card.links.push_back(link);
 	elif new_item is Limb:
 		preview_display = %NewLimbDisplay;
+		# preview_display.image = new_item["blob_images"];
+		var positioned_limb = PositionedLimb.new();
+		positioned_limb.limb = new_item;
+		positioned_limb.position = Vector2.ZERO;
+		display.card.blobs[display.card.blobs.size() - 1].limbs.push_back(positioned_limb);
 	preview_display.visible = true;
+	display.display_amalgam(display.card);
 	current_ix += 1;
 
 
 func _input(event: InputEvent) -> void:
+	if is_active == false:
+		return ;
 	if event is InputEventMouseMotion:
 		if preview_display != null:
 			preview_display.position = event.position;
@@ -58,5 +79,5 @@ func _input(event: InputEvent) -> void:
 			return ;
 		elif event.is_action_released("click"):
 			preview_display.modulate = Color(1, 1, 1, 1);
-			process_next_item();
+			process_next_item(event.global_position);
 			return ;
